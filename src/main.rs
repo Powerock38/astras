6d62fa562c8c76aada5c6bf003e6bdeda24d36b3
powerock <mat.papp@free.fr>
2023-08-17 22:44:33 +0200

    solar_system.rs separate file

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use astre::*;
 use bevy::{prelude::*, transform::TransformSystem};
 use dockable_on_astre::*;
 use marker::*;
-use rand::Rng;
 use ship::*;
+use solar_system::*;
 use utils::reparent_system;
 use worm::*;
 
@@ -12,6 +12,7 @@ mod constants;
 mod dockable_on_astre;
 mod marker;
 mod ship;
+mod solar_system;
 mod utils;
 mod worm;
 
@@ -43,67 +44,4 @@ fn setup_universe(
     materials: ResMut<Assets<ColorMaterial>>,
 ) {
     spawn_solar_system(commands, meshes, materials)
-}
-
-#[derive(Component)]
-pub struct SolarSystem;
-
-fn spawn_solar_system(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let radius = rand::thread_rng().gen_range((300.)..1000.);
-    let mass = rand::thread_rng().gen_range((100.)..3000.);
-    let position = Vec2::new(0., 0.);
-    let nb_children = rand::thread_rng().gen_range(3..=20);
-
-    commands
-        .spawn(SpatialBundle::default())
-        .insert(SolarSystem)
-        .with_children(|mut c| {
-            spawn_astre(
-                &mut c,
-                &mut meshes,
-                &mut materials,
-                0.,
-                radius,
-                mass,
-                position,
-                0.,
-                true,
-                nb_children,
-                0,
-            );
-        })
-        .with_children(|c| {
-            let radius = 10000.;
-
-            for _ in 0..10 {
-                let position = Vec2::new(
-                    rand::thread_rng().gen_range(-radius..radius),
-                    rand::thread_rng().gen_range(-radius..radius),
-                );
-
-                let size = rand::thread_rng().gen_range(10. ..=100.);
-                let speed = rand::thread_rng().gen_range(100. ..=1000.);
-                let length = rand::thread_rng().gen_range(1..=10);
-
-                let change_direction_every = rand::thread_rng().gen_range(0.1..=3.);
-
-                spawn_worm(
-                    c,
-                    &mut meshes,
-                    &mut materials,
-                    position,
-                    size,
-                    speed,
-                    length,
-                    change_direction_every,
-                );
-            }
-        })
-        .with_children(|c| {
-            setup_ship(c, meshes, materials);
-        });
 }
