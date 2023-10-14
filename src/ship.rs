ab14fa71d3956fb06231b4686213359e986925d2
Mattéo Pappalardo <mat.papp@free.fr>
2023-10-14 23:05:35 +0100

    camera enhancements

--- src/ship.rs
+++ src/ship.rs
@@ -6,9 +6,12 @@ use bevy::{
 use crate::background::*;
 use crate::dockable_on_astre::DockableOnAstre;
 
+const CAMERA_DOLLY_MAX_LENGTH: f32 = 0.05;
+
 #[derive(Component)]
 pub struct Ship {
-    speed: f32,
+    speed: Vec2,
+    thrust: f32,
 }
 
 pub fn setup_ship(
@@ -27,7 +30,8 @@ pub fn setup_ship(
     })
     .insert((
         Ship {
-            speed: 3000., // pixels per second
+            speed: Vec2::new(0., 0.),
+            thrust: 3000.,
         },
         DockableOnAstre::default(),
     ))
@@ -51,10 +55,10 @@ pub fn setup_ship(
 pub fn update_ship(
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Ship, &mut Transform)>,
+    mut query: Query<(&mut Ship, &mut Transform)>,
 ) {
-    for (ship, mut transform) in query.iter_mut() {
-        let mut movement = Vec3::new(0., 0., 0.);
+    for (mut ship, mut transform) in query.iter_mut() {
+        let mut movement = Vec2::new(0., 0.);
 
         if keyboard_input.any_pressed(vec![KeyCode::Left, KeyCode::Q]) {
             movement.x -= 1.;
@@ -69,7 +73,11 @@ pub fn update_ship(
             movement.y -= 1.;
         }
 
-        transform.translation += movement * ship.speed * time.delta_seconds();
+        let acceleration = movement * ship.thrust;
+
+        ship.speed += acceleration * time.delta_seconds();
+
+        transform.translation += (ship.speed * time.delta_seconds()).extend(0.);
     }
 }
 
@@ -78,18 +86,25 @@ pub fn update_camera(
     mut scroll_evr: EventReader<MouseWheel>,
     mut query: Query<&mut OrthographicProjection, With<Camera>>,
     mut background_query: Query<&mut Transform, With<Background>>,
+    ship_query: Query<&Ship>,
     window: Query<&Window>,
 ) {
     let window = window.single();
 
-    for scroll in scroll_evr.iter() {
-        for mut projection in query.iter_mut() {
-            projection.scale *= 1. + 2. * scroll.y * time.delta_seconds();
+    let mut projection = query.single_mut();
 
-            for mut transform in background_query.iter_mut() {
-                transform.scale =
-                    Vec3::new(window.width(), window.height(), 0.0) * projection.scale;
-            }
-        }
+    for scroll in scroll_evr.iter() {
+        projection.scale *= 1. + 2. * scroll.y * time.delta_seconds();
+    }
+
+    for mut transform in background_query.iter_mut() {
+        transform.scale = Vec3::new(window.width(), window.height(), 0.0)
+            * projection.scale
+            * (1. + 2. * CAMERA_DOLLY_MAX_LENGTH);
+    }
+
+    for ship in ship_query.iter() {
+        let dolly_offset = (ship.speed * 0.00001).clamp_length_max(CAMERA_DOLLY_MAX_LENGTH);
+        projection.viewport_origin = dolly_offset + Vec2::new(0.5, 0.5);
     }
 }
